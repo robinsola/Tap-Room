66203b9eda7c77232a6cdf6bc89b662a4ccf5609
Robin Sola <rsolarms@gmail.com>
2018-05-02 10:12:54 -0700

    add edit form for editing all inputs of each cider.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'the Cider House';
+  kegs: Keg[] = [
+    new Keg('Sweet Pear','Angry Orchard', 3, 4),
+    new Keg('Hoppy Grapefruit','Square Mile', 6,  7),
+    new Keg('Super Dry', 'Seattle Cider', 6, 6),
+    new Keg('Amber Apple', 'Woodchuck', 4, 5),
+    new Keg('Pineapple Oasis', 'Portland Cider Co', 10, 9)
+  ];
+  selectedKeg = null;
+  editKeg(clickedKeg) {
+    this.selectedKeg = clickedKeg;
+  }
+
+  finishEdit() {
+    this.selectedKeg = null;
+  }
+
 }
